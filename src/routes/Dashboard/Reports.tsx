119baa1a4d5a991d63ac97f0772de4bde796241f
Steven A. Matos <steven_matos@ymail.com>
2024-06-26 22:51:15 -0400

    create boilerplate screens

---
+++ src/routes/Dashboard/Reports.tsx
@@ -0,0 +1,16 @@
+import LoggedInNav from "../../components/LoggedInNav";
+
+const Reports = () => {
+  return (
+    <div>
+      <LoggedInNav />
+      <div className="relative isolate px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-20">
+          <h1>Reports</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reports;
---
+++ src/routes/Dashboard/Settings.tsx
@@ -0,0 +1,16 @@
+import LoggedInNav from "../../components/LoggedInNav";
+
+const Settings = () => {
+  return (
+    <div>
+      <LoggedInNav />
+      <div className="relative isolate px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-20">
+          <h1>Settings</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
