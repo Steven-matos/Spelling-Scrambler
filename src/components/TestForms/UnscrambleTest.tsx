43fc44251ee60a3627ad3943851e066eeaf8bc42
Steven A. Matos <steven_matos@ymail.com>
2024-08-19 18:34:57 -0400

    make sure the word is scrambled

--- src/components/TestForms/UnscrambleTest.tsx
+++ src/components/TestForms/UnscrambleTest.tsx
@@ -13,12 +13,19 @@ type UnscrambleTestProps = {
 };
 
 const scrambleWord = (word: string): string => {
-  const letters = word.split("");
-  for (let i = letters.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [letters[i], letters[j]] = [letters[j], letters[i]];
+  if (word.length <= 1) return word;
+
+  let scrambled = word;
+  while (scrambled === word) {
+    const letters = word.split("");
+    for (let i = letters.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [letters[i], letters[j]] = [letters[j], letters[i]];
+    }
+    scrambled = letters.join("");
   }
-  return letters.join("");
+
+  return scrambled;
 };
 
 const UnscrambleTest: React.FC<UnscrambleTestProps> = ({
